0c976d567cf63019cead59d2d629c27313ea1dac
K.DHANUSH GOUD <koyyadidhanushgoud@gmail.com>
2025-03-01 11:49:18 +0530

    navbar updated

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -7,21 +7,21 @@ function Home() {
         navigate('/Login');
 
     }
-  return (
-    <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/Dashboard">Edupoly</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <button onClick={handleLogout}>Log Out</button>
-    </ul>
-  </div>
-</nav>
-    </div>
-  )
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <Link to="/Dashbord">Edupoly</Link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <button onClick={handleLogout}>Log Out</button>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Home
